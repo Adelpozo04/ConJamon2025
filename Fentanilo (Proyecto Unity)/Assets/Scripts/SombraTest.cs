ce85cee7fab5559b8119fa308f24f3547501b3a9
SamuelGarciaBazan <samuga05@ucm.es>
2025-03-13 00:25:57 +0100

    testeo de input

---
+++ Fentanilo (Proyecto Unity)/Assets/Scripts/SombraTest.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class SombraTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void test (InputAction.CallbackContext context){
+
+        print("tu vieja");
+    }
+
+}
