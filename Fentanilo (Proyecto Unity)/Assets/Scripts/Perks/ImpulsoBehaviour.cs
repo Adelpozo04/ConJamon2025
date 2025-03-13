4bdbeed0b321804f1c28bb064f0eade834a1b224
MiguelNPB <miguelnp@ucm.es>
2025-03-13 16:39:49 +0100

    arreglado mierda

--- Fentanilo (Proyecto Unity)/Assets/Scripts/Perks/ImpulsoBehaviour.cs
+++ Fentanilo (Proyecto Unity)/Assets/Scripts/Perks/ImpulsoBehaviour.cs
@@ -5,71 +5,11 @@ using UnityEngine.UIElements.Experimental;
 using UnityEngine.InputSystem;
 using Unity.VisualScripting;
 
-[RequireComponent(typeof(PerkGrabable))]
+
 public class ImpulsoBehaviour : PerkBehaviour
 {
-    [SerializeField] float attractTime;
-
-
-    private Rigidbody2D playerRB;
-
-    bool active = false;
-
-    public override void ActivateEffect(GameObject player)
+    public override void ActivateEffect()
     {
-        //player.GetComponent<PlayerMovement>().enabled = false;
-
-        // desactivar gravedad y anular linearVelocity
-        playerRB = player.GetComponent<Rigidbody2D>();
-        //playerRB.linearVelocity = new Vector2(0, 0);
-        playerRB.gravityScale = 0;
-
-        StartCoroutine(AttractPlayer(playerRB.transform, transform.position, attractTime));
-    }
-
-    IEnumerator AttractPlayer(Transform playerTr, Vector3 endPos, float totalTime)
-    {
-        float time = 0;
-
-        Vector3 startPos = playerTr.position;
-
-        while (time < totalTime)
-        {
-            time += Time.deltaTime;
-
-            if ((time / totalTime) < 1)
-            {
-                Vector3 pos = Vector3.Lerp(startPos, endPos, Easing.InCirc(time / totalTime));
-
-                playerTr.position = pos;
-            }
-            
-
-            yield return null;
-        }
-
-        active = true;
-    }
-
-    private void FixedUpdate()
-    {
-        if (active)
-        {
-            Vector3 direccion = transform.position - playerRB.transform.position;
-
-            playerRB.AddForce(direccion.normalized, ForceMode2D.Impulse);
-        }
-
-    }
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    
+        gameObject.AddComponent<ImpulsoEffect>();
     }
 }
