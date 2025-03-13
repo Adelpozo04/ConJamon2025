743eea71a4e30ba0412d3a2f005007f0d95f4690
Luis <lparres@ucm.es>
2025-03-13 16:10:03 +0100

    Sistema de disparo terminado

---
+++ Fentanilo (Proyecto Unity)/Assets/Scripts/Shooting/Bullet.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Shoot shootScript;
+    private LayerMask collisionLayers;
+
+    private void Start() {
+        
+    }
+
+    public void InitializeBullet(Shoot shooter, float lifetime, LayerMask layers) {
+        shootScript = shooter;
+        collisionLayers = layers;
+        Destroy(gameObject, lifetime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        // Comprobamos que la layer del objeto contra el que chocamos sea una de las layers con las que debemos colisionar.
+        if (((1 << collision.gameObject.layer) & collisionLayers) != 0) {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy() {
+        if (shootScript != null) {
+            shootScript.OnBulletDestroyed();
+        }
+    }
+}
