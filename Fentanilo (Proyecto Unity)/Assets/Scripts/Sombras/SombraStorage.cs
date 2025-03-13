2bea86a5e4606bde7282e28e9b4ba2413407a2f3
Luis <lparres@ucm.es>
2025-03-13 10:54:09 +0100

    Input callback wrapper arreglado

--- Fentanilo (Proyecto Unity)/Assets/Scripts/Sombras/SombraStorage.cs
+++ Fentanilo (Proyecto Unity)/Assets/Scripts/Sombras/SombraStorage.cs
@@ -98,7 +98,9 @@ public class SombraStorage : MonoBehaviour
         customCallbackContext.canceled = callback.canceled;
         customCallbackContext.performed = callback.performed;
 
-        customCallbackContext.valueVector2 = callback.ReadValue<Vector2>();
+        if (callback.valueType == typeof(Vector2)) { 
+            customCallbackContext.valueVector2 = callback.ReadValue<Vector2>();
+        }
 
         return customCallbackContext; 
     }
