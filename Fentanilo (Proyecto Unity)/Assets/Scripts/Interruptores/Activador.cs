2cf18933cafa3b33de72d2805cf90f7309efbad3
Javalonso1 <javalo08@ucm.es>
2025-03-13 16:21:33 +0100

    Interruptores varios

--- Fentanilo (Proyecto Unity)/Assets/Scripts/Interruptores/Activador.cs
+++ Fentanilo (Proyecto Unity)/Assets/Scripts/Interruptores/Activador.cs
@@ -9,14 +9,25 @@ public abstract class Activador : MonoBehaviour
     [SerializeField] private Activable[] activables;
 
     /// <summary>
+    /// Variable que guarda el último estado del Activador
+    /// </summary>
+    private bool isPressed = false;
+
+    /// <summary>
     /// El método para enviar señal de activarse a todos los activables asociados a este activador.
     /// </summary>
     /// <param name="state">El estado de señal enviada. Ej. true -> abrir puerta, false -> cerrar puerta</param>
     protected void SendToActivables(bool state)
     {
+        isPressed = state;
         foreach (Activable act in activables)
         {
             act.Activar(state);
         }
+    }
+
+    protected void Switch()
+    {        
+        SendToActivables(!isPressed);
     }
 }
