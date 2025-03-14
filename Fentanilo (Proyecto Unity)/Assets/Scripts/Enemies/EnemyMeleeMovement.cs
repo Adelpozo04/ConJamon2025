cb1078a2815d6f7c2d7744b536c540a9758027f0
DiegoSanchezAlvarez <diegosanchezalvarez22@hotmail.com>
2025-03-14 22:38:27 +0100

    EnemyMelee Raycast

---
+++ Fentanilo (Proyecto Unity)/Assets/Scripts/Enemies/EnemyMeleeMovement.cs
@@ -0,0 +1,141 @@
+using UnityEngine;
+
+public class EnemyMeleeMovement : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+
+    [SerializeField] private MovementState _currentState; // Estado actual del movimiento
+    [SerializeField] private LayerMask _playerLayer; // Capa para detectar al jugador
+    
+    [SerializeField] private float _searchRange; // Distancia del Raycast
+    [SerializeField] private float _maxDistance; // Distancia máxima de seguimiento
+    [SerializeField] private float _speed; // Velocidad de movimiento
+    [SerializeField] private float _raycastDistance;  // Distancia del Raycast
+
+    private Transform _playerTransform; // Referencia al transform del jugador
+    private bool _lookingRight = true; // Variable para saber si el enemigo está mirando a la derecha
+
+    [SerializeField] private Transform[] _patrolPositions;
+    private int _randomNumber;
+    private float _distancePP = 0.25f; //Distancia en la que reconoce que llega al punto de patrullaje
+
+    public enum MovementState
+    {
+        Patrolling,
+        Following,
+    }
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _randomNumber = Random.Range(0, _patrolPositions.Length);
+    }
+
+    private void Update()
+    {
+        switch (_currentState)
+        {
+            case MovementState.Patrolling:
+                PatrollingState();
+                break;
+            case MovementState.Following:
+                FollowingState();
+                break;
+        }
+    }
+
+    private void PatrollingState()
+    {
+        FlipToTarget(_patrolPositions[_randomNumber].position);
+
+        Vector3 _newPos = new Vector3(_patrolPositions[_randomNumber].position.x, transform.position.y, transform.position.z);
+        transform.position = Vector2.MoveTowards(transform.position, _newPos, _speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, _patrolPositions[_randomNumber].position) < _distancePP)
+        {
+            _randomNumber = Random.Range(0, _patrolPositions.Length);
+        }
+
+        // Determinar la dirección del raycast: hacia la derecha o izquierda dependiendo de si el enemigo está mirando a la derecha
+        Vector2 direction = _lookingRight ? Vector2.right : Vector2.left;
+
+        // Lanzar un Raycast para detectar al jugador
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, _raycastDistance, _playerLayer);
+
+        // Dibujar el Raycast en la Scene para depuración
+        Debug.DrawRay(transform.position, direction * _raycastDistance, Color.red);
+
+        // Si el raycast golpea al jugador, cambia al estado de "Following"
+        if (hit.collider != null)
+        {
+            _playerTransform = hit.collider.transform;
+            _currentState = MovementState.Following;
+        }
+        else
+        {
+            _playerTransform = null;
+        }
+    }
+
+    public void FollowingState()
+    {
+        if (_playerTransform == null)
+        {
+            _currentState = MovementState.Patrolling;
+            return;
+        }
+
+        // Moverse hacia la posición del jugador
+        Vector3 _posToFollow = new Vector3(_playerTransform.position.x, transform.position.y, 0);
+        transform.position = Vector2.MoveTowards(transform.position, _posToFollow, _speed * Time.deltaTime);
+
+        Vector2 direction = _lookingRight ? Vector2.right : Vector2.left;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, _raycastDistance, _playerLayer);
+        Debug.DrawRay(transform.position, direction * _raycastDistance, Color.blue);
+
+        if (hit.collider != null)
+        {
+            _playerTransform = hit.collider.transform;
+        }
+        else
+        {
+            _playerTransform = null;
+        }
+
+        // Si el jugador se aleja demasiado, volver al estado de patrullaje
+        if (_playerTransform != null && Vector2.Distance(transform.position, _playerTransform.position) > _searchRange)
+        {
+            _currentState = MovementState.Patrolling;
+            _playerTransform = null;
+        }
+    }
+
+    private void FlipToTarget(Vector3 _target)
+    {
+        if (_target.x > transform.position.x && !_lookingRight)
+        {
+            _lookingRight = true;
+            _spriteRenderer.flipX = false; // Mirar a la derecha
+        }
+        else if (_target.x < transform.position.x && _lookingRight)
+        {
+            _lookingRight = false;
+            _spriteRenderer.flipX = true; // Mirar a la izquierda
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var playerMovement = collision.collider.gameObject.GetComponent<PlayerMovement>();
+
+        if (playerMovement != null)
+        {
+            if (playerMovement._recording)
+            {
+                playerMovement._controller.stopRecording();
+            }
+
+            Destroy(playerMovement.gameObject);
+        }
+    }
+}
