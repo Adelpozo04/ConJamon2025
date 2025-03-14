506ca00c7792d56ce5379b318ea2626d70052e93
SamuelGarciaBazan <samuga05@ucm.es>
2025-03-14 20:14:28 +0100

    yo queria hacer la trampa...

--- Fentanilo (Proyecto Unity)/Assets/Scripts/Ostaculos/PinchosComponent.cs
+++ Fentanilo (Proyecto Unity)/Assets/Scripts/Ostaculos/PinchosComponent.cs
@@ -2,18 +2,6 @@ using UnityEngine;
 
 public class PinchosComponent : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         var playerMovement = collision.collider.gameObject.GetComponent<PlayerMovement>();
---
+++ Fentanilo (Proyecto Unity)/Assets/Scripts/Ostaculos/TrampaComponent.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class TrampaComponent : MonoBehaviour
+{
+    /*
+        -bug con el rigidstatic al dejar de grabar
+        -bug camara a veces
+        -bug repeticion de sombras
+        
+     
+     */
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
