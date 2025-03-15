a5a11b2255e23623e1d86718813020662918ba86
MiguelNPB <miguelnp@ucm.es>
2025-03-15 04:24:10 +0100

    Subido escenaBase y nivel1 terminado

--- Fentanilo (Proyecto Unity)/Assets/Scripts/Interruptores/GoalController.cs
+++ Fentanilo (Proyecto Unity)/Assets/Scripts/Interruptores/GoalController.cs
@@ -13,6 +13,7 @@ public class GoalController : MonoBehaviour
         {
             //Sacar un mensaje de que has ganado
             //Fade out y cambiar de escena
+            other.gameObject.GetComponent<PlayerMovement>().enabled = false;
             LevelManager.Instance.Won();
         }
         else
