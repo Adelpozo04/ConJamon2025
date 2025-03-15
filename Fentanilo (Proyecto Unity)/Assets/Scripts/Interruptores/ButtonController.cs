0b713dd9a3b8421a4d4a825c7beac057511c14e1
MiguelNPB <miguelnp@ucm.es>
2025-03-15 20:58:23 +0100

    Nivel 2 completado

--- Fentanilo (Proyecto Unity)/Assets/Scripts/Interruptores/ButtonController.cs
+++ Fentanilo (Proyecto Unity)/Assets/Scripts/Interruptores/ButtonController.cs
@@ -3,10 +3,19 @@ using UnityEngine.Serialization;
 
 public class ButtonController : Activador
 {
+    [SerializeField] private Sprite unpressedSprite;
+    [SerializeField] private Sprite pressedSprite;
+    private SpriteRenderer renderer;
+
+
     //Para probar la funcionalidad del botón sin que esté hecho. ESTO ES TEMPORAL
     [FormerlySerializedAs("clicktestPress")][SerializeField] private bool clicktestPress;
     int numPisando = 0;
 
+    private void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -25,6 +34,8 @@ public class ButtonController : Activador
             if(numPisando == 0)
             {
                 SendToActivables(true);
+                renderer.sprite = pressedSprite;
+
                 PlayAudioSFX();
             }            
             numPisando++;
@@ -38,6 +49,9 @@ public class ButtonController : Activador
             if (numPisando == 1)
             {
                 SendToActivables(false);
+                renderer.sprite = unpressedSprite;
+
+
                 PlayAudioSFX();
             }
             numPisando--;           
